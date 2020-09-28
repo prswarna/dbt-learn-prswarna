97577330ffd911bfc8fa5fe86057463b0275fee6
Prabhakar Swarna <prabhakar.swarna@usaa.com>
2020-09-28 23:47:05 +0000

    source funtion

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -4,4 +4,4 @@ paymentmethod as payment_method,
 status as payment_status, 
 coalesce((amount/100),0) as amount, --convert to  dollars and zero if null
 created as created_at
-from raw.stripe.payment
+from {{source('stripe', 'payment')}} 
